24881080d0f8c59d018573d10c00990dcea0bfe8
pedrolopez <p.lopez.ch.hr@hotmail.com>
2023-09-28 11:36:52 +0200

    tests accesing and invoking private static methods by reflection;

--- src/xtra/reflectionTest/App.java
+++ src/xtra/reflectionTest/App.java
@@ -11,6 +11,7 @@ public class App {
 		Board board = new Board(30, 90, "wood");
 		Field[] boardFields = board.getClass().getDeclaredFields();
 		
+		System.out.println(Board.getHeader());
 		System.out.println(board.getMaterial());
 		
 		for (Field f: boardFields) {
@@ -19,9 +20,7 @@ public class App {
 				f.setAccessible(true);
 				try {
 					f.set(board, "vinil");
-				} catch (IllegalArgumentException e) {
-					e.printStackTrace();
-				} catch (IllegalAccessException e) {
+				} catch (IllegalArgumentException | IllegalAccessException e) {
 					e.printStackTrace();
 				}
 			}
@@ -36,16 +35,36 @@ public class App {
 			if (m.getName().equals("getMaterial")) {
 				try {
 					System.out.println(m.invoke(board));
-				} catch (IllegalAccessException e) {
-					e.printStackTrace();
-				} catch (IllegalArgumentException e) {
-					e.printStackTrace();
-				} catch (InvocationTargetException e) {
+				} catch (IllegalAccessException | IllegalArgumentException 
+						| InvocationTargetException e) {
 					e.printStackTrace();
 				}
 				//System.out.println(board.getMaterial());
 			}
+			
+			if (m.getName().equals("writeHelloOnBoard")) {
+				try {
+					m.setAccessible(true);
+					m.invoke(board);
+				} catch (IllegalAccessException | IllegalArgumentException 
+						| InvocationTargetException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			if (m.getName().equals("cleanAny")) { 
+				try {
+					m.setAccessible(true);
+					m.invoke(null);
+					
+				} catch (IllegalAccessException | IllegalArgumentException 
+						| InvocationTargetException e) {
+					e.printStackTrace();
+				}
+			}
 		}
+		
+		System.out.print(Board.getHeader());
 	}
 
 }
--- src/xtra/reflectionTest/Board.java
+++ src/xtra/reflectionTest/Board.java
@@ -4,29 +4,54 @@ public class Board {
 	private int height;
 	private int length;
 	private String material;
+	private static String header;
 	
 	public Board(int height, int length, String material) {
 		this.height = height;
 		this.length = length;
 		this.material = material;
+		header = "Reflection";
 	}
 	public int getHeight() {
 		return height;
 	}
+	
 	public void setHeight(int height) {
 		this.height = height;
 	}
+	
 	public int getLength() {
 		return length;
 	}
+	
 	public void setLength(int length) {
 		this.length = length;
 	}
+	
 	public String getMaterial() {
 		return material;
 	}
+	
 	public void setMaterial(String material) {
 		this.material = material;
+	}
+	
+	public static String getHeader() {
+		return header;
+	}
+	public static void setHeader(String header) {
+		Board.header = header;
+	}
+	
+	@SuppressWarnings("unused")
+	private void writeHeaderOnBoard () {
+		System.out.println("");
+	}
+	
+	@SuppressWarnings("unused")
+	private static String cleanAny () {
+		header = "()";
+		return header;
 	}
 	
 	
